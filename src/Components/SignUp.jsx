119b5f796875919e02a4266c01cfb19856256b8b
UFahath <u.zfahath49@gmail.com>
2025-03-12 16:29:33 +0530

    mod

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -4,11 +4,18 @@ import flightsitelogo from '../assets/images/flightsitelogo.jpeg'
  const SignUp = () => {
   return (
     <>
-    <div className="container d-flex p-5 mt-5 bg-success rounded-3" style={{height:"600px"}}>
-      <div className="mt-5 ms-5 me-5">
-        <img src={flightsitelogo} width={100} height={100} className="rounded-circle"/>
-
+    <div className="container shadow-lg bg-body-tertiary d-flex p-5 mt-5 rounded-3 border border-4" style={{height:"600px"}}>
+      <div className=" mx-sm-5 mt-5 mx-auto">
+        <img src={flightsitelogo} width={100} height={100} className="ms-4 rounded-circle"/>
+        <div className="d-flex" style={{width:"80px"}}>
+        <h3 className="text-wrap w-20 ms-3" style={{textIndent:"20px"}}>Tours Travels</h3>
+          <span className="fw-bolder text-primary" style={{fontSize:"3rem"}}>&</span>
+          </div>
+          <div className="text-center"style={{width:"150px"}}>
+          <p className="text-wrap fs-5 mt-3">"Your Perfect Trip Planned to Perfection"</p>
+          </div>
       </div>
+      <div className="offset-1 border border-right-3"></div>
     </div>
     </>
   )
